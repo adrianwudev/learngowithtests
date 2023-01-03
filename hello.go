236df5f218e8d41cf6e612909d61af0f86d83a2f
adrian <adrian657984@gmail.com>
2023-01-04 00:19:54 +0800

    HelloTest done

--- hello.go
+++ hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const enlishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return enlishHelloPrefix + name
 }
 
 func main() {

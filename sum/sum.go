c289992234dc3a57840236f6c93af98652fcc2a7
adrian <adrian657984@gmail.com>
2023-01-05 23:33:57 +0800

    TDD - Arrays, Slices

--- sum/sum.go
+++ sum/sum.go
@@ -7,3 +7,26 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	lenOfTheNumbers := len(numbersToSum)
+	sums := make([]int, lenOfTheNumbers)
+
+	for i, nums := range numbersToSum {
+		sums[i] = Sum(nums)
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, nums := range numbersToSum {
+		if len(nums) == 0 {
+			sums = append(nums, 0)
+		} else {
+			tail := nums[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}

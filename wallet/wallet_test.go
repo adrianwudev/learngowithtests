7bc4a948035e511297a04179e221d30847411026
adrian <adrian657984@gmail.com>
2023-01-11 00:31:18 +0800

    TDD - errors

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -1,40 +1,95 @@
 package wallet
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 )
 
-type BitCoin int
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+type Bitcoin int
 
 type Wallet struct {
-	balance BitCoin
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount BitCoin) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() BitCoin {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
 }
 
 type Stringer interface {
 	String() string
 }
 
-func (b BitCoin) String() string {
+func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
 }
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(BitCoin(10))
-	got := wallet.Balance()
 
-	want := BitCoin(10)
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+
+		assertNoError(t, err)
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(10)
+		wallet := Wallet{balance: startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err, ErrInsufficientFunds)
+		assertBalance(t, wallet, startingBalance)
+	})
+
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
 
 	if got != want {
 		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Error("got an error but didn't want")
+	}
+}
+
+func assertError(t testing.TB, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Error("wanted an error but didn't get one")
+	}
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
 	}
 }
